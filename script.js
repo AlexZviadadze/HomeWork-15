ff8ef84e505d0e9a2a5067a6ee2a5eeb7f3fbd40
AlexZviadadze <117842458+AlexZviadadze@users.noreply.github.com>
2023-01-24 13:49:36 +0400

    added 1-2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 5
 let  personArray = ['alex', 'vano', 'givi', 'noka', 'khatia'];
 
 let addArray = [...personArray];
@@ -17,3 +18,17 @@ console.log(personArray);
 //თავში წაშლა
 personArray.shift();
 console.log(personArray);
+
+//1
+
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+    
+}
+
+//2
+let i = 0;
+while (i < 50) {
+    console.log(i);
+    i++
+}
