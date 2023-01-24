75cf2386532247f4e92bb5c59fab464aaa3db65b
AlexZviadadze <117842458+AlexZviadadze@users.noreply.github.com>
2023-01-24 14:04:31 +0400

    added  N3

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
-// 5
-let  personArray = ['alex', 'vano', 'givi', 'noka', 'khatia'];
+// // 5
+// let  personArray = ['alex', 'vano', 'givi', 'noka', 'khatia'];
 
-let addArray = [...personArray];
+// let addArray = [...personArray];
 
-//თავში  დამატება
-addArray.unshift('giorgi');
-console.log(addArray);
+// //თავში  დამატება
+// addArray.unshift('giorgi');
+// console.log(addArray);
 
-//ბოლოში დამატება
-addArray.push('tamaza');
-console.log(addArray);
+// //ბოლოში დამატება
+// addArray.push('tamaza');
+// console.log(addArray);
 
-//ბოლოში წაშლა
-personArray.pop();
-console.log(personArray);
+// //ბოლოში წაშლა
+// personArray.pop();
+// console.log(personArray);
 
-//თავში წაშლა
-personArray.shift();
-console.log(personArray);
+// //თავში წაშლა
+// personArray.shift();
+// console.log(personArray);
 
-//1
+// //1
 
-for (let i = 0; i < 100; i++) {
-    console.log(i);
+// for (let i = 0; i < 100; i++) {
+//     console.log(i);
     
-}
+// }
 
-//2
-let i = 0;
-while (i < 50) {
-    console.log(i);
-    i++
-}
+// //2
+// let i = 0;
+// while (i < 50) {
+//     console.log(i);
+//     i++
+// }
+
+//3
+
+// let i = 0;
+
+// do {
+//     console.log(i);
+//     i++;
+// }while(i < 150);
